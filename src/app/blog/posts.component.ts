74b504b3158aa175569881f2b23f79152297e682
Neo Ighodaro <neo@creativitykills.co>
2017-08-30 13:32:20 +0100

    Update posts.component.ts

--- src/app/blog/posts.component.ts
+++ src/app/blog/posts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'posts',
+  selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
